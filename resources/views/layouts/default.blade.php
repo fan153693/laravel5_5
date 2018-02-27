aefe99e1cb4550a0e3453dc61c211a29cc071ae9
fan <1137796654@qq.com>
2018-02-27 17:13:46 +0000

    Finish layouts and routes

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,11 +1,19 @@
 <!DOCTYPE html>
 <html>
 <head>
-<title>@yield('title','Laravel5.5 APP') - Laravel5.5新手入门教程1</title>
+<title>@yield('title','Laravel5.5 APP') - Laravel5.5新手入门教程</title>
+<link rel="stylesheet" href="/css/app.css">
+
 </head>
 
 <body>
+  @include('layouts._header')
 
-@yield('content')
+     <div class="container">
+       <div class="col-md-offset-1 col-md-10">
+         @yield('content')
+         @include('layouts._footer')
+       </div>
+     </div>
 </body>
 </html>
