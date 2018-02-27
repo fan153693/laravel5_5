4cf9ac4ac529a69c08615f4d0552409f7ef014ee
fnn <1137796654@qq.com>
2018-02-27 07:52:13 +0000

    test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel --hello world
+                    Laravel5.5 --hello world
                 </div>
 
                 <div class="links">
