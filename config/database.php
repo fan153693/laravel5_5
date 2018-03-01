7a350f9cf2fdad44f2805efc3dc5086465defa37
fan <1137796654@qq.com>
2018-03-01 15:08:31 +0000

    Update database configuration

--- config/database.php
+++ config/database.php
@@ -1,5 +1,5 @@
 <?php
-
+$db_config = get_db_config();
 return [
 
     /*
@@ -13,7 +13,8 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => $db_config['connection'],
+    //'default' => env('DB_CONNECTION', 'mysql'),
 
     /*
     |--------------------------------------------------------------------------
@@ -56,11 +57,16 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            //'host' => env('DB_HOST', '127.0.0.1'),
+            //'port' => env('DB_PORT', '5432'),
+            //'database' => env('DB_DATABASE', 'forge'),
+            //'username' => env('DB_USERNAME', 'forge'),
+            //'password' => env('DB_PASSWORD', ''),
+            'host' => $db_config['host'],
+            'port' => env('DB_PORT','5432'),
+            'database' => $db_config['database'],
+            'username' => $db_config['username'],
+            'password' => $db_config['password'],
             'charset' => 'utf8',
             'prefix' => '',
             'schema' => 'public',
